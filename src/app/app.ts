b36ab518518ebfd74dd096fcc12c876d2d462e3a
nityashree <nityashree018@gmail.com>
2026-07-28 18:42:27 +0530

    Week 7 - Student Course Portal using Angular Components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Header } from './header/header';
+import { CourseList } from './course-list/course-list';
+import { StudentProfile } from './student-profile/student-profile';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    Header,
+    CourseList,
+    StudentProfile
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/course-list/course-list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-list.html',
+  styleUrl: './course-list.css'
+})
+export class CourseList {
+
+}
